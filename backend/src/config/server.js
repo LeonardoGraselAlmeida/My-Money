76df53b46ccee860deefba2bd6ecde95a839147c
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-06-19 15:33:37 -0300

    Adicionar os arquivos para configurar o backend do projeto

---
+++ backend/src/config/server.js
@@ -0,0 +1,18 @@
+const PORT = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
+
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser())
+
+server.listen(PORT, function(){
+    console.log(`BACKEND is running on port ${PORT}.`)
+})
+
+module.exports = server
