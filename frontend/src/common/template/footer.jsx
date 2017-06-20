72f0dd4ffbc479c133fd1360b5822de968f5756b
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-06-20 14:52:20 -0300

    Adicionar os componentes responsaveis sobre os templates principais

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>Copyright &copy; 2017
+            <a href="https://github.com/LeonardoGraselAlmeida" target='_blank'> Github - Leonardo Grasel Almeida</a>.
+        </strong>
+    </footer>
+)
