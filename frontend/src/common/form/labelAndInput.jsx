6bcc04d5f49374b07eefea3968b928dd6b2fb00f
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-06-21 15:11:41 -0300

    Adicionar component para facilitar o uso do input e label

---
+++ frontend/src/common/form/labelAndInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Grid from '../layout/grid'
+
+export default props => (
+    <Grid cols={props.cols}>
+        <div className='form-group'>
+            <label htmlFor={props.name}>{props.label}</label>
+            <input {...props.input} className='form-control'
+                placeholder={props.placeholder} readOnly={props.readOnly}
+                type={props.type} required={props.required}
+                maxLength={props.maxLength} min={props.min} max={props.max}/>
+        </div>
+    </Grid>
+)
