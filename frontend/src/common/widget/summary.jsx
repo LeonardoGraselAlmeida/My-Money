ec3e7fa34c9d75db3f16de82a4d21a834730fd3d
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-06-22 13:50:02 -0300

    Adicionar o component de Summary para ser reutilizavel

---
+++ frontend/src/common/widget/summary.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Row from '../layout/row'
+import ValueBox from './valueBox'
+
+export default ({ credit, debt }) => (
+    <Row>
+        <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de Créditos' />
+        <ValueBox cols='12 4' color='red' icon='bank' value={`R$ ${debt}`} text='Total de Débitos' />
+        <ValueBox cols='12 4' color='blue' icon='bank' value={`R$ ${credit - debt}`} text='Total Consolidado' />
+    </Row>
+)
