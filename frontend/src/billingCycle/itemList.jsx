067241b91cdb7496402a11b216bfed50ce0054c6
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-06-22 13:49:26 -0300

    Adicionar o field de Status que estava faltando para os debitos

--- frontend/src/billingCycle/itemList.jsx
+++ frontend/src/billingCycle/itemList.jsx
@@ -1,61 +1,73 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Grid from "../common/layout/grid";
-import {Field, arrayInsert, arrayRemove} from "redux-form";
-import {connect} from 'react-redux';
+import { Field, arrayInsert, arrayRemove } from "redux-form";
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import Input from '../common/form/input'
 import IconButton from '../common/widget/iconButton'
+import If from '../common/operador/if'
 
-class ItemList extends Component{
-    add(index, item={}){
-        if(!this.props.readOnly){
+class ItemList extends Component {
+    add(index, item = {}) {
+        if (!this.props.readOnly) {
             this.props.arrayInsert('billingCycleForm', this.props.field, index, item)
         }
     }
 
-    remove(index){
-        if(!this.props.readOnly && this.props.list.length > 1){
+    remove(index) {
+        if (!this.props.readOnly && this.props.list.length > 1) {
             this.props.arrayRemove('billingCycleForm', this.props.field, index)
         }
     }
 
-    renderRows(){
+    renderRows() {
         const list = this.props.list || [];
 
         return list.map((item, index) => (
-                <tr key={index}>
-                    <td><Field name={`${this.props.field}[${index}].name`} component={Input}
-                               placeholder="Informe o nome" readOnly={this.props.readOnly}/></td>
-                    <td><Field name={`${this.props.field}[${index}].value`} component={Input}
-                               placeholder="Informe o valor" readOnly={this.props.readOnly}/></td>
-                    <td><IconButton btn="success" type="button" icon="plus"
-                                    onClick={()=>this.add(index+1)}/>
-                        <IconButton btn="warning" type="button" icon="clone"
-                                    onClick={()=>this.add(index+1, item)}/>
-                        <IconButton btn="danger" type="button" icon="trash-o"
-                                    onClick={()=>this.remove(index)}/></td>
-                </tr>
-            )
+            <tr key={index}>
+                <td>
+                    <Field name={`${this.props.field}[${index}].name`} component={Input}
+                        placeholder="Informe o nome" readOnly={this.props.readOnly} /></td>
+                <td>
+                    <Field name={`${this.props.field}[${index}].value`} component={Input}
+                        placeholder="Informe o valor" readOnly={this.props.readOnly} /></td>
+                <If test={this.props.showStatus}>
+                    <td>
+                        <Field name={`${this.props.field}[${index}].status`} component={Input}
+                            placeholder="Informe o status" readOnly={this.props.readOnly} />
+                    </td>
+                </If>
+                <td>
+                    <IconButton btn="success" type="button" icon="plus"
+                        onClick={() => this.add(index + 1)} />
+                    <IconButton btn="warning" type="button" icon="clone"
+                        onClick={() => this.add(index + 1, item)} />
+                    <IconButton btn="danger" type="button" icon="trash-o"
+                        onClick={() => this.remove(index)} />
+                </td>
+            </tr>
+        )
         )
 
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <Grid cols={this.props.cols}>
                 <fieldset>
                     <legend>{this.props.legend}</legend>
                     <table className="table">
                         <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Valor</th>
-                            <th>Ações</th>
-                        </tr>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Valor</th>
+                                <If test={this.props.showStatus}><th>Status</th></If>
+                                <th className='table-actions'>Ações</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        { this.renderRows() }
+                            {this.renderRows()}
                         </tbody>
                     </table>
                 </fieldset>
@@ -64,5 +76,5 @@ class ItemList extends Component{
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert, arrayRemove}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch);
 export default connect(null, mapDispatchToProps)(ItemList)
