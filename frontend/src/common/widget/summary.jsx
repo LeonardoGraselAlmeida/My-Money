35b91c034ba0fcd54e5caed3d1cccb62c12959f2
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-06-22 22:26:29 -0300

    Ajustar o icone a ser utilizado no summary

--- frontend/src/common/widget/summary.jsx
+++ frontend/src/common/widget/summary.jsx
@@ -6,7 +6,7 @@ import ValueBox from './valueBox'
 export default ({ credit, debt }) => (
     <Row>
         <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de Créditos' />
-        <ValueBox cols='12 4' color='red' icon='bank' value={`R$ ${debt}`} text='Total de Débitos' />
-        <ValueBox cols='12 4' color='blue' icon='bank' value={`R$ ${credit - debt}`} text='Total Consolidado' />
+        <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de Débitos' />
+        <ValueBox cols='12 4' color='blue' icon='money' value={`R$ ${credit - debt}`} text='Total Consolidado' />
     </Row>
 )
